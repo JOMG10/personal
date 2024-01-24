b0a900a0096a7839650400a91a0ff30fdb22ab90
JOMG10 <116333088+JOMG10@users.noreply.github.com>
2024-01-23 23:41:51 -0600

    ..

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -6,14 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import {Tab1PageModule} from "../tab1/tab1.module";
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    TabsPageRoutingModule
-  ],
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        TabsPageRoutingModule,
+        Tab1PageModule
+    ],
   declarations: [TabsPage]
 })
 export class TabsPageModule {}
