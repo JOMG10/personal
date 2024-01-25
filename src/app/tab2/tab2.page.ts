041488a13780e4db9066e889b119445c119bf132
JOMG10 <116333088+JOMG10@users.noreply.github.com>
2024-01-25 14:38:47 -0600

    ..

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,51 +9,11 @@ import { ModalController } from '@ionic/angular';
 })
 export class Tab2Page {
   constructor(private modalCtrl: ModalController) {}
-
-  nombre = {
-    nombre: "jose",
-    apellido: "martiez"
-  }
-
-  deudas = [
-    {
-      nombre:"PAGOS ACREIMEX",
-      cantidad : 250,
-      fecha : 10
-    },
-    {
-      nombre:"PAGOS COPPEL",
-      cantidad : 100,
-      fecha : 10
-    },
-    {
-      nombre:"PAGOS ACREIMEX",
-      cantidad : 1000,
-      fecha : 10
-    },
-    {
-      nombre:"PAGOS ACREIMEX",
-      cantidad : 200,
-      fecha : 10
-    }
-  ]
-
- /*
-  contenido:Carrito[] = [];
-  carro : Carrito = {
-    nombre:'',
-    descripcion:'',
-    tallas:"",
-    precio: 0,
-    colores: '',
-    rutaImagen:'',
-    id:this.firestore.getId()
-  };
-
-  */
+  deudas:any[] = []
+  nuevoDato: any ={}
   isModalOpen = false;
   isModalItem =false;
-
+  name= true
 
 
   setOpen(isOpen: boolean) {
@@ -60,15 +22,16 @@ export class Tab2Page {
   setOpenItem(openItem : boolean){
     this.isModalItem = openItem;
   }
-
-  name = true
-
-  cancel() {
-    return this.modalCtrl.dismiss(null, 'cancel');
-  }
-
   confirm() {
     return this.modalCtrl.dismiss(this.name, 'confirm');
   }
-
+  agregarPago() {
+    this.deudas.push( {
+      nombre:this.nuevoDato.nombre,
+      cantidad:this.nuevoDato.cantidad,
+      fecha:this.nuevoDato.fecha
+    })
+    this.setOpen(false)
+    this.nuevoDato = {}
+  }
 }
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -8,6 +8,7 @@ import { TabsPageRoutingModule } from './tabs-routing.module';
 import { TabsPage } from './tabs.page';
 import {Tab1PageModule} from "../tab1/tab1.module";
 import {Tab2PageModule} from "../tab2/tab2.module";
+import {Tab3PageModule} from "../tab3/tab3.module";
 
 @NgModule({
     imports: [
@@ -16,7 +17,8 @@ import {Tab2PageModule} from "../tab2/tab2.module";
         FormsModule,
         TabsPageRoutingModule,
         Tab1PageModule,
-        Tab2PageModule
+        Tab2PageModule,
+        Tab3PageModule
     ],
   declarations: [TabsPage]
 })
